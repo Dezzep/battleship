55c9cc02ce987c81ac995aaad9ef418e7f84708d
Jacob Handelman <jacobhandelman@gmail.com>
2022-05-27 09:17:35 -0400

    refactored the ShipMaker class --> turned ShipMaker into a factory functions --> ensured that the tests for ShipMaker were appropriately altered.

--- src/shipmaker.js
+++ src/shipmaker.js
@@ -1,33 +1,30 @@
-export default class ShipMaker {
-  constructor(shipLength) {
-    this.shipLength = shipLength;
-    this.damage = [];
-    this.defeated = false;
-  }
-
-  get possibleHits() {
-    return this.determinePossibleHits();
-  }
-
-  determinePossibleHits = () => {
-    const possibleHits = [];
-    for (let i = 0; i < this.shipLength; i += 1) {
+const ShipMaker = (len) => {
+  const shipLength = len;
+  const shipsDamage = {};
+  shipsDamage.damage = [];
+  shipsDamage.defeated = false;
+  const possibleHits = [];
+  const determinePossibleHits = () => {
+    for (let i = 0; i < shipLength; i += 1) {
       possibleHits.push(i);
     }
     return possibleHits;
   };
-
-  isSunk = (dmg, shipLen) => {
+  const isSunk = (dmg, shipLen) => {
     if (dmg.length === shipLen) {
-      this.defeated = true;
+      shipsDamage.defeated = true;
     }
   };
-
-  hit = (num) => {
-    if (!this.damage.includes(num)) {
-      this.damage.push(num);
+  const hit = (num) => {
+    if (!shipsDamage.damage.includes(num)) {
+      shipsDamage.damage.push(num);
       // Do Stuff To The Dom Function? --
     }
-    this.isSunk(this.damage, this.shipLength);
+    isSunk(shipsDamage.damage, shipLength);
   };
-}
+  return {
+    possibleHits: determinePossibleHits(), hit, isSunk, shipLength, shipsDamage,
+  };
+};
+
+export { ShipMaker };
--- src/shipmaker.test.js
+++ src/shipmaker.test.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
-import ShipMaker from './shipmaker';
+import { ShipMaker } from './shipmaker';
 
-const coolBattleShip = new ShipMaker(3);
+const coolBattleShip = ShipMaker(3);
 afterEach(() => {
-  coolBattleShip.damage = [];
+  coolBattleShip.shipsDamage.damage = [];
 });
 test('when all parts of ship are hit, the ship sinks', () => {
   coolBattleShip.hit(0);
@@ -11,17 +11,17 @@ test('when all parts of ship are hit, the ship sinks', () => {
   coolBattleShip.hit(0);
   coolBattleShip.hit(2);
   coolBattleShip.hit(1);
-  expect(coolBattleShip.defeated).toBeTruthy();
+  expect(coolBattleShip.shipsDamage.defeated).toBeTruthy();
 });
 test('hit function can have no duplicates', () => {
   coolBattleShip.hit(2);
   coolBattleShip.hit(2);
-  expect(coolBattleShip.damage).toStrictEqual([2]);
+  expect(coolBattleShip.shipsDamage.damage).toStrictEqual([2]);
 });
 
 test('hit function pushes to damage array', () => {
   coolBattleShip.hit(2);
-  expect(coolBattleShip.damage).toStrictEqual([2]);
+  expect(coolBattleShip.shipsDamage.damage).toStrictEqual([2]);
 });
 test('possible hits is [0 1 2]', () => {
   const arr = [0, 1, 2];
