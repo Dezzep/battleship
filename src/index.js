d7674817f521f4d6a6907949ffd4adbc804eb1a9
Jacob Handelman <jacobhandelman@gmail.com>
2022-06-01 13:07:22 -0400

    refactored the way I generate a grid to the dom so that it uses already established objects to generate it which will make it easier to determine what has a ship and what doesnt

--- src/index.js
+++ src/index.js
@@ -20,41 +20,97 @@ aiBoard.addShipToTable(aiBoard.shipPieces[4], aiBoard.table.I, 7);
 
 const human = Player(player1Board, aiBoard);
 
-console.log(player1Board);
-console.log(aiBoard);
-
-const createPlayerBoard = (DivID) => {
-  // creates the grid along with the alphebetical Y axis and the numerical X axis.
-  const boardDiv = document.createElement('div');
-  const lettersForColumns = ['numbers', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
-  boardDiv.id = DivID;
-  lettersForColumns.forEach((element, i) => {
-    const createChildDiv = document.createElement('div');
-    createChildDiv.className = `column ${element}`;
-    boardDiv.append(createChildDiv);
-    lettersForColumns.forEach((e, index) => {
-      const createGrandChildDiv = document.createElement('div');
-      if (i === 0) {
-        if (index !== 0) {
-          createGrandChildDiv.innerText = index;
-        }
-        createGrandChildDiv.className = 'numbers';
-      }
-      if (index - 1 === -1) {
-        createGrandChildDiv.className = 'identify';
-        if (element !== 'numbers') {
-          createGrandChildDiv.innerText = element;
-        }
-      } else if (createGrandChildDiv.className !== 'numbers') {
-        createGrandChildDiv.className = index - 1;
-      }
-      createChildDiv.append(createGrandChildDiv);
+const appendPlayerBoardToDom = () => {
+  const mainDiv = document.createElement('div');
+  mainDiv.id = 'players-game-board';
+  const tableObject = human.board.table;
+  Object.keys(tableObject).forEach((key, v) => {
+    const createDiv = document.createElement('div');
+    createDiv.innerText = key;
+    createDiv.className = key;
+    const row = Object.values(tableObject)[v];
+    row.forEach((tile) => {
+      const tileDiv = document.createElement('div');
+      tileDiv.innerText = tile;
+      createDiv.append(tileDiv);
     });
+    mainDiv.append(createDiv);
   });
-  document.body.append(boardDiv);
-};
+  // Object.values(tableObject).forEach((value) => {
+  //   for (let i = 0; i < value.length; i += 1) {
+  //     const childDiv = document.createElement('div');
+  //     childDiv.innerText = value[i];
+  //     childDiv.className = 'x-coord';
+  //     createDiv.append(childDiv);
+  //     console.log(value[i]);
+  //   }
+  //   console.log(value);
+  // });
 
-const prettyMuchDoEverythingDomRelatedAllAtOnceInANiceAndDescriptiveFunctionThatMightBeAFunctionThatsJustTurningIntoAJoke = () => {
-  createPlayerBoard('players-game-board');
+  document.body.append(mainDiv);
 };
-prettyMuchDoEverythingDomRelatedAllAtOnceInANiceAndDescriptiveFunctionThatMightBeAFunctionThatsJustTurningIntoAJoke();
+appendPlayerBoardToDom();
+// const createPlayerBoard = (DivID) => {
+//   // creates the grid along with the alphebetical Y axis and the numerical X axis.
+//   const boardDiv = document.createElement('div');
+//   const lettersForColumns = ['numbers', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+//   boardDiv.id = DivID;
+//   lettersForColumns.forEach((element, i) => {
+//     const createChildDiv = document.createElement('div');
+//     createChildDiv.className = `column ${element}`;
+//     boardDiv.append(createChildDiv);
+//     lettersForColumns.forEach((e, index) => {
+//       const createGrandChildDiv = document.createElement('div');
+//       if (i === 0) {
+//         if (index !== 0) {
+//           createGrandChildDiv.innerText = index;
+//         }
+//         createGrandChildDiv.className = 'numbers';
+//       }
+//       if (index - 1 === -1) {
+//         createGrandChildDiv.className = 'identify';
+//         if (element !== 'numbers') {
+//           createGrandChildDiv.innerText = element;
+//         }
+//       } else if (createGrandChildDiv.className !== 'numbers') {
+//         createGrandChildDiv.className = index - 1;
+//       }
+//       createChildDiv.append(createGrandChildDiv);
+//     });
+//   });
+//   document.body.append(boardDiv);
+// };
+
+// const prettyMuchDoEverythingDomRelatedAllAtOnceInANiceAndDescriptiveFunctionThatMightBeAFunctionThatsJustTurningIntoAJoke = () => {
+//   createPlayerBoard('players-game-board');
+// };
+// prettyMuchDoEverythingDomRelatedAllAtOnceInANiceAndDescriptiveFunctionThatMightBeAFunctionThatsJustTurningIntoAJoke();
+
+// const player1BoardTiles = document.querySelectorAll('#players-game-board > .A > div, #players-game-board .B > div,#players-game-board .C > div, #players-game-board .D > div,#players-game-board .E > div,#players-game-board .F > div,#players-game-board .G > div,#players-game-board .H > div,#players-game-board .I > div,#players-game-board .J > div');
+// const splitPlayer1BoardTilesInto10 = (board) => {
+  
+// }
+// const linkBattleShipPositionToDom = () => {
+//   player1BoardTiles.forEach((element) => {
+//     if (element.className !== 'identify') {
+//       console.log(element);
+//     }
+//   })
+//   const tableObject = human.board.table
+//   Object.keys(tableObject).forEach(key => {
+//     console.log(key, tableObject[key]);
+//   });
+// };
+
+// const changeAColor = () => {
+
+//   player1BoardTiles.forEach((element) => {
+//     element.addEventListener('click', () => {
+//       if (element.className !== 'identify') {
+//         element.style.background = 'black';
+//       }
+//     });
+//   });
+// };
+// changeAColor();
+// linkBattleShipPositionToDom();
